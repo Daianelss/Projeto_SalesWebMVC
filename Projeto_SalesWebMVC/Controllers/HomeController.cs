7cde5f3dfbf1de1d3f47449e16493f45f21b6fb7
Daianelss <daianelucena@hotmail.com>
2021-02-04 16:34:58 -0300

    First controller and Razor pages tests

--- Projeto_SalesWebMVC/Controllers/HomeController.cs
+++ Projeto_SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace Projeto_SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio ALves";
+
 
             return View();
         }
